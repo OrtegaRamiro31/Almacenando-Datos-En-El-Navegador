15ec0d2707dcec228f53d27986380e3a1738599a
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2022-12-23 13:27:12 -0600

    Separación de tareas por fecha terminado.

--- components/addTask.js
+++ components/addTask.js
@@ -1,5 +1,6 @@
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
+import { displayTasks } from './readTasks.js';
 
 export const addTask = (evento) => {
     evento.preventDefault();
@@ -19,13 +20,15 @@ export const addTask = (evento) => {
     const taskObj = {
         value,
         dateFormat
-    }
+    };
+
+    list.innerHTML = '';
     const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
     taskList.push(taskObj);
   
     localStorage.setItem("tasks", JSON.stringify(taskList));
-    const task = createTask(taskObj);
-    list.appendChild(task);
+
+    displayTasks(); 
   };
   
   export const createTask = ({value, dateFormat}) => {
